c6638f2510d0d651b148c179713e94c4e424a611
gizmore <gizmore@wechall.net>
2018-11-18 00:50:22 +0100

    de

---
+++ lang/recovery_de.php
@@ -0,0 +1,31 @@
+<?php
+return array(
+# Sidebar
+'btn_recovery' => 'Passwort Vergessen?',
+
+# Forgot?
+'ft_recovery_form' => '[%s] Passwort zurücksetzen',
+'msg_recovery_mail_sent' => 'Wir haben Ihnen eine E-Mail gesendet die weiter Anweisungen enthält.',
+'err_email_or_login' => 'Bitte geben Sie an welches Konto Sie zurücksetzen wollen.',
+	
+# Change
+'ft_recovery_change' => '[%s] Passwort ändern',
+'new_password' => 'Neues Passwort',
+'password_retype' => 'Erneut eingeben',
+'err_password_retype' => 'Sie müssen das Passwort zwei mal eingeben.',
+'msg_pass_changed' => 'Ihr Passwort wurde erfolgreich geändert.',
+############################################################
+'mail_subj_recovery' => '[%s] Passwort ändern',
+'mail_subj_body' => '
+Hallo %s,
+
+Sie möchten Ihr Passwort bei %s ändern?
+
+Folgen Sie einfach diesem Link oder ignorieren Sie diese Mail.
+
+%s
+
+Viele Grüße,
+Das %2$s Team
+',
+);
